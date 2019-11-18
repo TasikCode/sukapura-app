14b279010961521faa847c2b98c345080f9e3d09
sanghiang77 <7sanghiang@gmail.com>
2019-11-18 21:26:42 +0700

    App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import Router from './src/config/router/index';
+
+export default class Home extends Component {
+  render() {
+    return <Router />;
+  }
+}
